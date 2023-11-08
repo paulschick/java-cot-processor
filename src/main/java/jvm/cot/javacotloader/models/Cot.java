fafdc359185a8f7d4c037cd642da38f9f998a564
paulschick <paulschick19@gmail.com>
2023-11-08 18:50:01 -0500

    implement lombok for entity

--- src/main/java/jvm/cot/javacotloader/models/Cot.java
+++ src/main/java/jvm/cot/javacotloader/models/Cot.java
@@ -1,10 +1,14 @@
 package jvm.cot.javacotloader.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
+@Getter @Setter @NoArgsConstructor
 public class Cot {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,84 +31,4 @@ public class Cot {
     private String nonReptLong;
     @Column(name = "non_rept_short")
     private String nonReptShort;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getMarket() {
-        return market;
-    }
-
-    public void setMarket(String market) {
-        this.market = market;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getOpenInterest() {
-        return openInterest;
-    }
-
-    public void setOpenInterest(String openInterest) {
-        this.openInterest = openInterest;
-    }
-
-    public String getNonCommLong() {
-        return nonCommLong;
-    }
-
-    public void setNonCommLong(String nonCommLong) {
-        this.nonCommLong = nonCommLong;
-    }
-
-    public String getNonCommShort() {
-        return nonCommShort;
-    }
-
-    public void setNonCommShort(String nonCommShort) {
-        this.nonCommShort = nonCommShort;
-    }
-
-    public String getCommLong() {
-        return commLong;
-    }
-
-    public void setCommLong(String commLong) {
-        this.commLong = commLong;
-    }
-
-    public String getCommShort() {
-        return commShort;
-    }
-
-    public void setCommShort(String commShort) {
-        this.commShort = commShort;
-    }
-
-    public String getNonReptLong() {
-        return nonReptLong;
-    }
-
-    public void setNonReptLong(String nonReptLong) {
-        this.nonReptLong = nonReptLong;
-    }
-
-    public String getNonReptShort() {
-        return nonReptShort;
-    }
-
-    public void setNonReptShort(String nonReptShort) {
-        this.nonReptShort = nonReptShort;
-    }
 }
