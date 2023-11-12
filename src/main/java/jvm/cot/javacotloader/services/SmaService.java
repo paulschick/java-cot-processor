be42c87005fa20e2c644668d4084232e94db9010
paulschick <paulschick19@gmail.com>
2023-11-12 16:09:06 -0500

    Create method to get Cots with SMA values from db

--- src/main/java/jvm/cot/javacotloader/services/SmaService.java
+++ src/main/java/jvm/cot/javacotloader/services/SmaService.java
@@ -24,10 +24,30 @@ public class SmaService {
         this.cotRepository = cotRepository;
     }
 
+    public List<Map<String, Object>> getCotsWithSma(int period, String market) {
+        String smaKey = "sma" + period;
+        Collection<Cot> cotsCollection = cotRepository.retrieveByMarket(market);
+        List<Cot> cots = new ArrayList<>(cotsCollection.stream().toList());
+        List<Map<String, Object>> response = new ArrayList<>();
+        logger.info("Retrieved " + cots.size() + " records for market " + market);
+        for (Cot cot : cots) {
+            Map<String, Object> cotMap = new HashMap<>();
+            SimpleMovingAverage sma = smaRepository.findByCotIdAndPeriod(cot.getId(), period);
+            cotMap.put("cot", cot);
+            if (sma != null) {
+                cotMap.put(smaKey, sma.getValue());
+            } else {
+                cotMap.put(smaKey, 0);
+            }
+            response.add(cotMap);
+        }
+        return response;
+    }
+
     public void insertSmaForPeriodMarket(int period, String market) {
         try {
             Collection<Cot> cotsCollection = cotRepository.retrieveByMarket(market);
-            List<Cot> cots = new java.util.ArrayList<>(cotsCollection.stream().toList());
+            List<Cot> cots = new ArrayList<>(cotsCollection.stream().toList());
             logger.info("Retrieved " + cots.size() + " records for market " + market);
             cots.sort(Comparator.comparing(Cot::getDate));
             List<Map<String, Object>> smaCots = calculateMovingAverage(cots, Cot::getOpenInterest, period);
