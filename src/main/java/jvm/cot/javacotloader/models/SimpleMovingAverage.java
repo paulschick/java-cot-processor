b9c5e41b77047f76bcd719b2a3b8f6defa5e86e7
paulschick <paulschick19@gmail.com>
2023-11-11 20:25:39 -0500

    Create sma entity and repository

---
+++ src/main/java/jvm/cot/javacotloader/models/SimpleMovingAverage.java
@@ -0,0 +1,20 @@
+package jvm.cot.javacotloader.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class SimpleMovingAverage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "cot_id")
+    private Long cotId;
+    @Column(name = "period")
+    private int period;
+    @Column(name = "value")
+    private String value;
+}
